53769f880ddf2b39af22191a2911e0e0a879504a
ferbarbar1 <ferbarbar1@alum.us.es>
2024-05-01 11:59:18 +0200

    fix:added allowed host

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["http://localhost:5173/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "http://localhost:5173/",
+    "localhost",
+    "127.0.0.1",
+    "https://tfgbackend-production.up.railway.app/",
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
